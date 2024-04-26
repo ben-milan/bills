8e6e85a1fb7e72455b47605562db337e09ec6c63
Ben-Milan <klenkb@bzz.ch>
2024-04-26 13:46:12 +0200

    fixed the delete via a fetch to backend with the unique id of the bill

--- public/src/entry_management.js
+++ public/src/entry_management.js
@@ -1,21 +1,32 @@
-const fs = require("fs")
-
 const express = require("express")
+const session = require("express-session")
+const data = require("../data/data.json")
+const fs = require("fs")
 
 const router = express.Router()
 
 router.use(express.static("public"))
 router.use(express.urlencoded({ extended: true }))
 router.use(express.json())
+router.use(session({
+    secret: 'supersecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {}
+}))
 
-router.delete("/delete", (req, res) => {
-    console.log(req.body)
-
-    updatedData = JSON.stringify(req.body, null, 2)
-    fs.writeFileSync("./public/data/data.json", updatedData)
-    res.status(200)
-    // refresh the site
-
+router.delete("/delete/:id", (req, res) => {
+    const id = String(req.params.id)
+    const idx = data.findIndex(entry => String(entry.id) === String(id))
+        
+    if (idx !== -1) {
+        data.splice(idx, 1)
+        updatedData = JSON.stringify(data, null, 2)
+        fs.writeFileSync("./public/data/data.json", updatedData)
+        res.status(200).json({Success: "Data deleted and updated successfully."})
+    } else {
+        res.status(404).send(`Data with id [${id}] has not been found.`)
+    }
 })
 
 module.exports = router
--- public/src/statusHandler.js
+++ public/src/statusHandler.js
@@ -1,42 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.addEventListener("click", (event) => {
-        fetch("../data/data.json")
-        .then(res => res.json())
-        .then(data => {
-            const data_for_del = data.filter((entry) => String(entry.user) === String(emailFromBack));
-            const data_without_usr = data.filter((entry) => String(entry.user) !== String(emailFromBack));
-            console.log(data_for_del)
 
-            if (event.target.classList.contains("status")) {
-                const button = event.target
-                id = Number(button.getAttribute("id")) -1
-                data_for_del.splice(id, 1)
-                data_for_del.forEach(element => {
-                    data_without_usr.push(element)
-                });
-                console.log("Data:", data_without_usr)
+        if (event.target.classList.contains("status")) {
+            const button = event.target
+            id = String(button.getAttribute("id"))
+            console.log(id)
 
-                fetch("/delete", {
-                    method: "DELETE",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(data_without_usr)
+            fetch(`/delete/${id}`, {
+                method: "DELETE",
                 })
-                .then(res => {
-                    if (res.ok) {
-                        console.log("Data sent to backend.")
-                    } else {
-                        console.error("Failed to send data.")
-                    }
-                })
-                .catch(err => {
-                    console.error("Error sending data to backend:", err)
-                })
+            .then(res => {
+                if (res.status === 200) {
+                    history.go()
+                    console.log("ID sent to backend.")
+                } else {
+                    console.error("Failed to send data.")
+                }
+            })
+            .catch(err => {
+                console.error("Error sending data to backend:", err)
+            })
 
-                button.classList.remove("btn-danger")
-                button.classList.add("btn-success")
-            }
-    })
+            button.classList.remove("btn-danger")
+            button.classList.add("btn-success")
+        }
     })
 })
